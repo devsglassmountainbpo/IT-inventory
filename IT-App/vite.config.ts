788a9a8630578973f997a71aa22cd5473fa591fe
jcontreras07 <jcontreras@glassmountainbpo.com>
2024-06-26 16:55:49 -0600

    test

--- IT-App/vite.config.ts
+++ IT-App/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: 'https://devs.glassmountainbpo.com/',
+  base: 'https://inv.glassmountainbpo.com/',
   plugins: [react()],
   assetsInclude: ['**/*.xlsx'],
 });
